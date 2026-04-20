e010808d177c60cbcf272ed20fbb086955cc581e
daremove <daremove1@gmail.com>
2026-04-20 12:20:19 +0500

    initial commit

---
+++ test/shop-react-redux-cloudfront-be.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ShopReactReduxCloudfrontBe from '../lib/shop-react-redux-cloudfront-be-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/shop-react-redux-cloudfront-be-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ShopReactReduxCloudfrontBe.ShopReactReduxCloudfrontBeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
